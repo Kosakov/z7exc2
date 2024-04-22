f0968e161b2e7bf4bcef0b8b80beaed0ed7c4ab2
Kosakov <nikolaikosakov@gmail.com>
2024-04-22 22:43:29 +0200

    NavBar fixes

--- src/app/components/layout/navBar.tsx
+++ src/app/components/layout/navBar.tsx
@@ -3,21 +3,23 @@ import logo from "/public/Roooby.svg";
 
 function NavBar() {
   return (
-    <nav>
-      <Image priority src={logo} alt="Follow us on Twitter" />
-      <div>
-        <a href="#">Product</a>
-        <a href="#">Pricing</a>
-        <a href="#">Company</a>
-        <a href="#">Recourses</a>
-        <a href="#">Contact</a>
+    <nav className="w-100% h-auto  flex justify-around items-center mt-8">
+      <Image priority src={logo} alt="Logooo"/>
+      <div className=" text-sm font-medium flex gap-9 ">
+        <a href="#" className="hover:text-gray-500">Product</a>
+        <a href="#" className="hover:text-gray-500">Pricing</a>
+        <a href="#" className="hover:text-gray-500">Company</a>
+        <a href="#" className="hover:text-gray-500">Recourses</a>
+        <a href="#" className="hover:text-gray-500">Contact</a>
       </div>
-      <button className=" border-2 border-[#DCDCDC] text-xs bg-[#FFFF] text-black tracking-[-0.19px] rounded-sm py-3 px-6 font-bold leading-4 ">
+      <div className="flex gap-5">
+      <button className="hover:bg-gray-300  border-2 border-[#DCDCDC] text-xs bg-[#FFFF] text-black tracking-[-0.19px] rounded-md py-3 px-6 font-bold leading-4 ">
         Log In
       </button>
-      <button className=" text-xs bg-[#150050] text-white tracking-[-0.19px] rounded-sm py-3 px-6 font-bold leading-4 ">
+      <button className=" text-xs bg-[#150050] text-white tracking-[-0.19px] rounded-md py-3 px-6 font-bold leading-4 ">
         Try for free
       </button>
+      </div>
     </nav>
   );
 }
